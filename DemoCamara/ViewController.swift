15889075e08d3691343076a05413db5dbf9746e4
sofiia-0 <ana.sofiia02@gmail.com>
2024-09-21 14:37:46 -0600

    Initial Commit

---
+++ DemoCamara/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCamara
+//
+//  Created by LABORATORIO MAC UAM on 21/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
